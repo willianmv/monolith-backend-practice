7164c461234c325b02df8db376aaa36a1ce217ce
willianmv <willianvmarques300@gmail.com>
2025-11-04 18:01:09 -0300

    feat(core): adiciona interfaces de repositório
    - Cria contratos para persistencia das entidades
    - Define operações básicas de CRUD e consultas específicas no domínio

---
+++ backend/src/main/java/com/simple/blog/backend/core/gateway/repository/IPostRepository.java
@@ -0,0 +1,15 @@
+package com.simple.blog.backend.core.gateway.repository;
+
+import com.simple.blog.backend.core.domain.DomainPage;
+import com.simple.blog.backend.core.domain.Post;
+import com.simple.blog.backend.core.usecases.post.summary.PostQueryFilter;
+import com.simple.blog.backend.core.usecases.post.summary.SummaryPostOutput;
+
+public interface IPostRepository {
+
+    Post save(Post post);
+
+    Post findById(long id);
+
+    DomainPage<SummaryPostOutput> findSummaryPosts(PostQueryFilter filter, int page, int size);
+}
---
+++ backend/src/main/java/com/simple/blog/backend/core/gateway/repository/IValidationCodeRepository.java
@@ -0,0 +1,15 @@
+package com.simple.blog.backend.core.gateway.repository;
+
+import com.simple.blog.backend.core.domain.ValidationCode;
+
+public interface IValidationCodeRepository {
+
+    void save(ValidationCode code);
+
+    ValidationCode findByCode(String code);
+
+    boolean existsValidByUserId(long userId);
+
+    ValidationCode findLatestByUserId(long userId);
+
+}
