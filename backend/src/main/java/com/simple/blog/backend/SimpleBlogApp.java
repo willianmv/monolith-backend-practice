e602f3a818c3c1db2772e9afb78fa7a7b3b0f1f3
willianmv <willianvmarques300@gmail.com>
2025-11-04 17:39:32 -0300

    config: classe de inicializaçao da aplicação spring

---
+++ backend/src/main/java/com/simple/blog/backend/SimpleBlogApp.java
@@ -0,0 +1,14 @@
+package com.simple.blog.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class SimpleBlogApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBlogApp.class, args);
+	}
+
+}
